0f33df8b95b98a26a5f79f6be4a0b88e8a8c6120
zivana afra yulianto <afrazivana5@gmail.com>
2025-04-15 23:43:06 +0700

    Membuat variabel boolean dengan nama yang jelas untuk status pernikahan dan menggunakan logic yang lebih eksplisit.

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -12,26 +12,27 @@ public class Employee {
 	private String lastName;
 	private String idNumber;
 	private String address;
-	
+
 	private int yearJoined;
 	private int monthJoined;
 	private int dayJoined;
 	private int monthWorkingInYear;
-	
+
 	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
-	
+	private boolean gender; // true = Laki-laki, false = Perempuan
+
 	private int monthlySalary;
 	private int otherMonthlyIncome;
 	private int annualDeductible;
-	
+
 	private String spouseName;
 	private String spouseIdNumber;
 
 	private List<String> childNames;
 	private List<String> childIdNumbers;
-	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address,
+			int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -42,16 +43,18 @@ public class Employee {
 		this.dayJoined = dayJoined;
 		this.isForeigner = isForeigner;
 		this.gender = gender;
-		
+
 		childNames = new LinkedList<String>();
 		childIdNumbers = new LinkedList<String>();
 	}
-	
+
 	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
+	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya
+	 * (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3:
+	 * 7.000.000 per bulan)
 	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
 	 */
-	
+
 	private static final int GAJI_GRADE_1 = 3000000;
 	private static final int GAJI_GRADE_2 = 5000000;
 	private static final int GAJI_GRADE_3 = 7000000;
@@ -70,36 +73,39 @@ public class Employee {
 			monthlySalary = (int) (monthlySalary * PERSENTASE_KARYAWAN_ASING);
 		}
 	}
-	
-	public void setAnnualDeductible(int deductible) {	
+
+	public void setAnnualDeductible(int deductible) {
 		this.annualDeductible = deductible;
 	}
-	
-	public void setAdditionalIncome(int income) {	
+
+	public void setAdditionalIncome(int income) {
 		this.otherMonthlyIncome = income;
 	}
-	
+
 	public void setSpouse(String spouseName, String spouseIdNumber) {
 		this.spouseName = spouseName;
 		this.spouseIdNumber = spouseIdNumber;
 	}
-	
+
 	public void addChild(String childName, String childIdNumber) {
 		childNames.add(childName);
 		childIdNumbers.add(childIdNumber);
 	}
-	
+
 	public int getAnnualIncomeTax() {
-		
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
+
+		// Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah
+		// bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
 		LocalDate date = LocalDate.now();
-		
+
 		if (date.getYear() == yearJoined) {
 			monthWorkingInYear = date.getMonthValue() - monthJoined;
-		}else {
+		} else {
 			monthWorkingInYear = 12;
 		}
-		
-		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
+
+		boolean sudahMenikah = spouseName != null && !spouseName.isEmpty();
+		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible,
+				sudahMenikah, childIdNumbers.size());
 	}
 }
